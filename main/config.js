f99f3a51f4ac71685690c57e6ac2bcabafd32624
Michael Wen Xuan Xie <35471954+aeriamike@users.noreply.github.com>
2019-10-07 20:57:10 -0700

    Add files via upload

---
+++ main/config.js
@@ -0,0 +1,61 @@
+const config = {
+    encoding:"GBK",
+
+    max_number : 10,
+
+
+    showMessage : true,
+
+    auto_sort : true,
+
+    reverse: false,
+
+    use_custom_color : true,
+
+    itemLabel : "The most popular tag on Pixiv:",
+    typeLabel : "",
+
+    item_x : 800,
+
+    interval_time : 1,
+
+
+    text_y : -50,
+
+    display_barInfo : 100,
+
+
+    use_type_info : true,
+
+    divide_by_type: true,
+
+
+    use_counter : false,
+
+    step : 7,
+
+
+    format : '.0f',
+
+
+    left_margin : 230,
+    right_margin : 150,
+    top_margin : 200,
+    bottom_margin : 0,
+
+
+    dateLabel_x : 800,
+    dateLabel_y : 750,
+
+
+    allow_up : true,
+
+
+    enter_from_0: false,
+
+
+    big_value: false,
+
+    use_semilogarithmic_coordinate: false
+
+}
